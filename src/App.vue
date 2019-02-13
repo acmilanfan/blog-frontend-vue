c81b61c4d39dfce5e0cb396720e3d99721a8a763
acmilanfan <gentoo1993@gmail.com>
2019-02-13 16:20:58 +0300

    change codestyle

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Header/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <Header/>
+    </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+    import Header from './components/Header.vue'
 
-export default {
-  name: 'app',
-  components: {
-    Header
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            Header
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
