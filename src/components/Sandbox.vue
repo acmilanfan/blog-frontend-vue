dcf036540a983515eed903c221f9e08cec1116cd
acmilanfan <gentoo1993@gmail.com>
2019-02-27 16:13:42 +0300

    Add a theme switcher to the footer

--- src/components/Sandbox.vue
+++ src/components/Sandbox.vue
@@ -39,6 +39,7 @@
         </v-content>
         <v-footer app>
             <span class="px-3">&copy; {{ new Date().getFullYear() }}</span>
+            <v-switch v-model="dark" primary label="Dark"></v-switch>
         </v-footer>
     </v-app>
 </template>
